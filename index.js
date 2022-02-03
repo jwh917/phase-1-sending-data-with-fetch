d63b4856aeb2cadbbc79c3e5c886115675305b24
Julien Hnery <jwh_917@yahoo.com>
2022-02-03 06:27:19 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+const formData = {
+  name: "Steve",
+  email: "steve@steve.com",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+function submitData(name, email){
+    const dataSubmit = fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (object) {
+        const display = document.querySelector('body')
+        display.innerHTML = object.id
+    })
+    .catch(function (error) {
+        const display = document.querySelector('body')
+        alert("Error!");
+        display.innerHTML = error.message;
+    });
+    return dataSubmit;
+}
